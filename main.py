5051124ac085fba022e74d1de33e5d4305485484
M.vin <emeka.vinan@gmail.com>
2019-10-10 14:54:59 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+""""
+@author: Vin-Anuonye Emeka
+
+
+"""
+import sys
+from Formatter.OutFormatter_track1 import Openseseme
+from Formatter.OutFormatter_track2 import Openseseme2
+
+
+if __name__=="__main__":
+    Toopen = Openseseme()
+    Topen2= Openseseme2()
+    Toopen.Open3()
+    Topen2.Open3()
+
